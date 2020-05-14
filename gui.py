a4b19371f98082ab5b26b9d2d96bb24e9b0c83e8
MustafaKhalil-IST <mustafa.samir.khalil@tecnico.ulisboa.pt>
2020-05-14 02:12:14 +0100

    gui: show players, points, deck

--- gui.py
+++ gui.py
@@ -11,18 +11,73 @@ from pygame.locals import (
     KEYDOWN,
     QUIT,
 )
+from game import Game
 
 pygame.init()
 
 # Define constants for the screen width and height
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
 
 # Create the screen object
 # The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+
+def draw_card(card, t):
+    image = pygame.image.load(r'./DECK/{}.gif'.format(card))
+    screen.blit(image, t)
+
+
+def show_players_numbers(rotate=1):
+    positions = [(0, 300), (400, 0), (400, 600), (800, 300)]
+    for i in range(1, 5):
+        font = pygame.font.SysFont('Arial', 25)
+        screen.blit(font.render('A{}'.format(i), True, (255, 0, 0)), positions[(i - 1 + rotate) % 4])
+
+
+def display_all_cards(deck):
+    positions = [(i + 300, 400) for i in range(10, 200, 30)] + [(i + 310, 500) for i in range(10, 180, 30)]
+    for i, pos in enumerate(positions):
+        draw_card(deck[i], pos)
+    pass
+
+
+def show_player_cards(player):
+    # TODO
+    pass
+
+
+def show_table():
+    # TODO
+    pass
+
+
+def remove_card_from_player(player):
+    # TODO
+    pass
+
+
+def show_round_number(round):
+    # TODO
+    pass
+
+
+def show_points():
+    positions = [(850, 600), (850, 650), (850, 700), (850, 750)]
+    font = pygame.font.SysFont('Arial', 25)
+    screen.blit(font.render('Points', True, (0, 0, 255)), (850, 550))
+    for i in range(1, 5):
+        screen.blit(font.render('A{}: {}'.format(i, 0), True, (0, 0, 255)), positions[i - 1])
+
+
+def run_game():
+    # TODO
+    pass
+
+
 # Run until the user asks to quit
+game = Game()
 running = True
 while running:
     # Did the user click the window close button?
@@ -40,9 +95,11 @@ while running:
 
     # Fill the background with white
     screen.fill((255, 255, 255))
-
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    # pygame.draw.rect(screen, (0, 0, 255), (200, 100, 100, 50))
+    # pygame.draw.lines(screen, (0, 0, 0), False, [[10, 80], [40, 80], [40, 100], [10, 100], [10, 80]], 2)
+    show_players_numbers()
+    display_all_cards(deck=13 * ["2c"])
+    show_points()
 
     # Flip the display
     pygame.display.flip()
--- qlearning.py
+++ qlearning.py
@@ -4,26 +4,25 @@ from keras.optimizers import RMSprop
 import numpy as np
 import pandas as pd
 
+from agent import Agent
 
-class Learner:
+
+class Learner(Agent):
     def __init__(self):
         super().__init__()
         self._Q = {}
-        self._last_state = None
-        self._last_action = None
-        self._learning_rate = .7
-        self._discount = .9
-        self._epsilon = .9
-        self._learning = True
-        self._hand = []
+        self.last_state = None
+        self.last_action = None
+        self.learning_rate = .7
+        self.discount = .9
+        self.epsilon = .9
 
     def reset_hand(self):
-        self._hand = []
-        self._last_state = None
-        self._last_action = None
+        self.last_state = None
+        self.last_action = None
 
     def get_action(self, state):
-        if state in self._Q and np.random.uniform(0, 1) < self._epsilon:
+        if state in self._Q and np.random.uniform(0, 1) < self.epsilon:
             action = max(self._Q[state], key=self._Q[state].get)
         else:
             action = np.random.choice([])  # TODO
@@ -31,22 +30,21 @@ class Learner:
                 self._Q[state] = {}
             self._Q[state][action] = 0
 
-        self._last_state = state
-        self._last_action = action
+        self.last_state = state
+        self.last_action = action
 
         return action
 
     def update(self, new_state, reward):
-        if self._learning:
-            old = self._Q[self._last_state][self._last_action]
+        old = self._Q[self.last_state][self.last_action]
 
-            if new_state in self._Q:
-                new = self._discount * self._Q[new_state][max(self._Q[new_state], key=self._Q[new_state].get)]
-            else:
-                new = 0
+        if new_state in self._Q:
+            new = self.discount * self._Q[new_state][max(self._Q[new_state], key=self._Q[new_state].get)]
+        else:
+            new = 0
 
-            self._Q[self._last_state][self._last_action] = (1 - self._learning_rate) * old + self._learning_rate * (
-                        reward + new)
+        self._Q[self.last_state][self.last_action] = (1 - self.learning_rate) * old + self.learning_rate * (
+                    reward + new)
 
     def get_optimal_strategy(self):
         df = pd.DataFrame(self._Q).transpose()
@@ -81,10 +79,10 @@ class DQNAgent(Learner):
         rms = RMSprop()
         model.compile(loss='mse', optimizer=rms)
 
-        self._model = model
+        self.model = model
 
     def get_action(self, state):
-        rewards = self._model.predict([np.array([state])], batch_size=1)
+        rewards = self.model.predict([np.array([state])], batch_size=1)
 
         if np.random.uniform(0, 1) < self._epsilon:
             if rewards[0][0] > rewards[0][1]:
@@ -101,21 +99,19 @@ class DQNAgent(Learner):
         return action
 
     def update(self, new_state, reward):
-        if self._learning:
-            rewards = self._model.predict([np.array([new_state])], batch_size=1)
-            maxQ = rewards[0][0] if rewards[0][0] > rewards[0][1] else rewards[0][1]
-            new = self._discount * maxQ
+        rewards = self.model.predict([np.array([new_state])], batch_size=1)
+        maxQ = rewards[0][0] if rewards[0][0] > rewards[0][1] else rewards[0][1]
+        new = self._discount * maxQ
 
-            if self.last_action == 0:  # TODO
-                self.last_target[0][0] = reward + new
-            else:
-                self.last_target[0][1] = reward + new
+        if self.last_action == 0:  # TODO
+            self.last_target[0][0] = reward + new
+        else:
+            self.last_target[0][1] = reward + new
 
-            # Update model
-            self._model.fit(np.array([self.last_state]), self.last_target, batch_size=1, nb_epoch=1, verbose=0)
+        # Update model
+        self.model.fit(np.array([self.last_state]), self.last_target, batch_size=1, nb_epoch=1, verbose=0)
 
     def get_optimal_strategy(self):
-
         index = []
         for x in range(0, 21):
             for y in range(1, 11):
@@ -124,7 +120,7 @@ class DQNAgent(Learner):
         df = pd.DataFrame(index=index, columns=['hit', 'stay'])
 
         for ind in index:
-            outcome = self._model.predict([np.array([ind])], batch_size=1)
+            outcome = self.model.predict([np.array([ind])], batch_size=1)
             df.loc[ind, 'hit'] = outcome[0][0]
             df.loc[ind, 'stay'] = outcome[0][1]
 
